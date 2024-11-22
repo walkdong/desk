53ea7d66ec695876df9ca2f935c5fe2d09c16d30
Kinari <Kinari@alnilam.online>
2024-11-22 21:23:11 +0800

    add Alnilam.online

---
+++ Alnilam.online/js.js
@@ -0,0 +1,13 @@
+alert("nihao")
+document.write("<h1>我是来自js的一级标签</h1>")
+console.log("来自js的console.log")
+let num
+num=prompt("请输入数据:")
+console.log("num= ",num)
+let arr=["one","two","three"]
+console.log("arr.length: ",arr.length)
+console.log(`num的数据类型是: ${typeof num}`)
+let float1=123.23
+console.log("float1=123.23 可保留小数：",parseFloat(float1))
+console.log("folat1=123.23 只保留正数：",parseInt(float1))
+//parseint 可以把“12px”变为12
